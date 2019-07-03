ae0a8b23666818d0caf89e46a578c431c90b7aa4
Manami Ito <manami.j27@gmail.com>
2019-07-03 22:09:38 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
@@ -10,12 +9,36 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials/')
 
-
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
 
+app.get("/beers", (req, res) => {
+ punkAPI.getBeers()
+ .then(beers =>{
+  // console.log(beers)
+    res.render("beers", { beerList: beers });
+  })
+  .catch(error => {
+    res.send(error);
+  });
+});
+
+
+app.get('/randombeer', (req, res) => {
+  
+  punkAPI
+  .getRandom()
+  .then(beers => {
+    res.render('randombeer', {random: beers});
+  })
+  .catch(error => {
+    res.send(error);
+  });
+  
+});
 
 app.listen(3000);
